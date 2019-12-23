a43a9ae98c6b09034575efd2477496591263082e
JEONGSUJI <wtw9611@gmail.com>
2019-12-23 18:33:54 +0900

    admin 추가

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post, PostImage, PostComment, PostLike
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PostComment)
+class PostCommentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PostLike)
+class PostLikeAdmin(admin.ModelAdmin):
+    pass
